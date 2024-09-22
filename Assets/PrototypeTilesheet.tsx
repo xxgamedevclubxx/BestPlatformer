8d8ac04f22c25221fbe80114f807ae6b86690deb
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-21 19:57:33 -0500

    Add Garrett physics, Tiled map imports

---
+++ Assets/PrototypeTilesheet.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="PrototypeTilesheet" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="PrototypeTilesheet.png" width="512" height="512"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+   <object id="2" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Scripts/Player.cs
@@ -0,0 +1,116 @@
+using System;
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    [Export]
+    private RayCast2D rightRay,
+        leftRay;
+
+    [Export]
+    private Timer coyoteTimer,
+        jumpBufferTimer,
+        wallJumpTimer,
+        wallJumpCoyoteTimer;
+
+    // Movement settings
+    private const float Speed = 250.0f;
+    private const float SlideSpeed = 1750.0f;
+    private const float JumpVelocity = -750.0f;
+    private const float WallJumpHorizontalVelocity = 750.0f;
+    private const float WallJumpVerticalVelocity = -750.0f;
+    private const float Gravity = 800.0f;
+    private const float WallSlideSpeed = 100.0f;
+
+    private bool isWallSliding,
+        isJumpingFromWall;
+
+    private RayCast2D lastRayColliding = null;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+        bool WallLeft = leftRay.IsColliding();
+        bool WallRight = rightRay.IsColliding();
+
+        if (WallLeft)
+        {
+            lastRayColliding = leftRay;
+        }
+        else if (WallRight)
+        {
+            lastRayColliding = rightRay;
+        }
+
+        // Add the gravity.
+        if (!IsOnFloor() && !isWallSliding)
+        {
+            velocity += GetGravity() * (float)delta;
+        }
+
+        //Wall sliding
+        if (IsOnWall() && velocity.Y > 0 && !IsOnFloor())
+        {
+            isWallSliding = true;
+            velocity.Y = WallSlideSpeed;
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+
+        // Handle horizontal movement
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        velocity.X =
+            direction != Vector2.Zero && wallJumpTimer.IsStopped()
+                ? direction.X * Speed
+                : Mathf.MoveToward(Velocity.X, 0, SlideSpeed * (float)delta);
+
+        //Handle Wall Jump, timer included so the jump away from the wall is not cut too short.
+        if (Input.IsActionJustPressed("ui_accept"))
+        {
+            // Floor jump condition
+            if (IsOnFloor() || coyoteTimer.TimeLeft > 0.0)
+            {
+                velocity.Y = JumpVelocity;
+            }
+            // Wall jump condition
+            else if (lastRayColliding != null && (IsOnWall() || wallJumpCoyoteTimer.TimeLeft > 0.0))
+            {
+                isJumpingFromWall = true;
+                wallJumpTimer.Start();
+                velocity.Y = WallJumpVerticalVelocity;
+                if (lastRayColliding == leftRay)
+                {
+                    velocity.X = WallJumpHorizontalVelocity;
+                }
+                else
+                {
+                    velocity.X = -WallJumpHorizontalVelocity;
+                }
+
+                lastRayColliding = null;
+            }
+        }
+
+        if (IsOnFloor())
+        {
+            isJumpingFromWall = false;
+        }
+
+        Velocity = velocity;
+        bool wasOnFloor = IsOnFloor();
+        bool wasOnWall = IsOnWall();
+        MoveAndSlide();
+
+        // Start coyote timer if the player just left a ledge
+        if (wasOnFloor && !IsOnFloor() && velocity.Y >= 0)
+        {
+            coyoteTimer.Start();
+        }
+        if (wasOnWall && !IsOnWall())
+        {
+            wallJumpCoyoteTimer.Start();
+        }
+    }
+}
